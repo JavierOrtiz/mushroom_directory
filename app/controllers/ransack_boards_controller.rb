3ba562d1a750d80d1684d6615f12b80ed1293ee1
Javier <jortiz.dev@gmail.com>
2020-02-15 17:20:34 +0100

    Prepare views for ransack actions

---
+++ app/controllers/ransack_boards_controller.rb
@@ -0,0 +1,45 @@
+class ElasticBoardsController < ApplicationController
+  before_action :column_names, :filters_constructor
+  def index
+    @mushrooms = mushrooms_search
+  end
+
+  def create
+    @mushrooms = mushrooms_search
+  end
+
+  private
+
+  def mushrooms_search
+    Mushroom.search("*", where: query_params, page: params[:page], per_page: 50, aggs: column_names.map(&:to_sym), load: false)
+  end
+
+  def filters_constructor
+    result = {}
+    mushrooms_search.aggregations.each do |aggregation|
+      parsed_aggregations = parse_aggregation(aggregation)
+      next if parsed_aggregations.empty?
+
+      result[aggregation[0].to_sym] = parsed_aggregations
+    end
+    @filters = result
+  end
+
+  def parse_aggregation(agg)
+    result = []
+    buckets = (agg[1]["buckets"] || agg[1].dig(agg[0], "buckets"))
+    buckets.each do |key|
+      result << { t("agaricus_lepiota.#{agg[0]}.#{key['key']}") => key['key']}
+    end
+    result
+  end
+
+  def column_names
+    @table_column_names = Mushroom.column_names.reject { |cn| cn.in? ["created_at", "updated_at", "id"] }
+  end
+
+  def query_params
+    @permitted_params ||= params.permit(column_names)
+    @query_params ||= @permitted_params.delete_if { |_key, value| value.blank? }
+  end
+end
